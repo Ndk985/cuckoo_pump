f377be3871456dc3619a008b178ef29427dcf52a
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-11-07 18:11:01 +0300

    тест деплоя

---
+++ test.py
@@ -0,0 +1,6 @@
+# Test file for GitHub Actions workflow
+# This file is used to test file copying to the server
+# Created: 2025-11-07
+
+print("This is a test file for verifying file copy functionality")
+
